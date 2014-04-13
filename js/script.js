7a3672530c9e316fe3d02767aa58ab61d8e27e7f
puhrome <phirom.yim@fullsail.edu>
2014-04-13 16:17:07 -0400

    Concatenating
    Using WebStorm to concatenate word strings and number string in HTML
    and Java

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Concatenating (Combing Strings) Created by Phirom Yim on 4/13/14.
+ */
+//two or more strings you need to combine
+var firsName = "Phirom";
+var lastName = "Yim";
+var fullName = firstName + " " +LastName;
+
+//using concatenation to add space between name
+
+console.log (fullName);
+
+var a = "6";//concatenating into a string
+var b = "7";
+
+var result = a + b; //concatenate doesn't add var but instead combines
+console.log(result);//concatenating gives a string of "67" and not the number 13
